6426fc789ae182e6c272fb98a1d575a2f64d0db8
Tri040422 <huynhminhtri20041@gmail.com>
2024-10-03 10:55:56 +0700

    BookShopBee

--- src/index.js
+++ src/index.js
@@ -1,28 +1,54 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { store } from "./redux/store";
-import { Provider } from "react-redux";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
+// import { store } from "./redux/store";
+// import { Provider } from "react-redux";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-const queryClient = new QueryClient();
-root.render(
-  // <React.StrictMode>
-  <QueryClientProvider client={queryClient}>
-    <Provider store={store}>
-      <App />
-    </Provider>
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// const queryClient = new QueryClient();
+// root.render(
+//   // <React.StrictMode>
+//   <QueryClientProvider client={queryClient}>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
 
-    <ReactQueryDevtools initialIsOpen={false}></ReactQueryDevtools>
-  </QueryClientProvider>
-  // </React.StrictMode>
-);
+//     <ReactQueryDevtools initialIsOpen={false}></ReactQueryDevtools>
+//   </QueryClientProvider>
+//   // </React.StrictMode>
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const bodyParser = require("body-parser");
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+app.use(bodyParser.json());
+
+routes(app);
+
+mongoose
+  .connect(`${process.env.MONGO_DB}`)
+  .then(() => {
+    console.log("Connect DB success!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(port, () => {
+  console.log("Server is running in port: ", +port);
+});
